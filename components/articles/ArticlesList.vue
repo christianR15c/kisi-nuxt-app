9dc79b99ee9ed937597fbaa83e4d31a1262fc0c8
christian <habinezac15@gmail.com>
2022-07-25 20:07:59 +0200

    ft: filter by category added

--- components/articles/ArticlesList.vue
+++ components/articles/ArticlesList.vue
@@ -4,21 +4,36 @@
       <input
         type="text"
         placeholder="search"
-        class="border-2 px-2 rounded-xl"
+        class="border-2 px-2 rounded-2xl py-1"
         v-model="input"
       />
-      <div>
-        <!-- <div>
-          <p>filter by category</p>
-          <div
-            v-for="(article, i) in data.data.filter((article) =>
-              Object.keys(article)
-            )"
-            :key="i"
+    </div>
+    <div class="flex justify-center gap-3 pt-4">
+      <p>filter by category:</p>
+      <div class="flex gap-2">
+        <div
+          v-for="(article, i) in Array.from(
+            new Set(data.data.map((a) => a.category))
+          ).map((id) => {
+            return data.data.find((a) => a.category === id);
+          })"
+          :key="i"
+        >
+          <p
+            class="px-2 border-2 rounded-xl text-gray-400 cursor-pointer"
+            :class="article.category === selected && 'filtered-category'"
+            @click="onSelect(article.category)"
           >
-            <p>{{ article }}</p>
-          </div>
-        </div> -->
+            {{ article.category }}
+          </p>
+        </div>
+        <p
+          class="px-2 border-2 rounded-xl text-gray-400 cursor-pointer"
+          :class="selected === 'all' && 'filtered-category'"
+          @click="selected = 'all'"
+        >
+          All
+        </p>
       </div>
     </div>
     <div class="flex w-full justify-center">
@@ -42,7 +57,7 @@
     </div>
     <div class="flex w-full justify-center">
       <div
-        v-if="data"
+        v-if="selected === 'all'"
         :class="
           input
             ? 'hidden'
@@ -51,6 +66,46 @@
       >
         <ArticlesArticlecard
           v-for="(article, i) in data.data"
+          :key="i"
+          :title="article.title"
+          :description="article.description"
+          :image="article.image"
+          :category="article.category"
+          :author="article.author"
+          :date="article.published_at"
+        />
+      </div>
+    </div>
+    <div class="flex w-full justify-center">
+      <div
+        v-if="data"
+        :class="
+          input || selected
+            ? 'hidden'
+            : `w-full flex flex-wrap justify-center gap-3 h-full m-4 pb-8 max-w-[1240px]`
+        "
+      >
+        <ArticlesArticlecard
+          v-for="(article, i) in data.data"
+          :key="i"
+          :title="article.title"
+          :description="article.description"
+          :image="article.image"
+          :category="article.category"
+          :author="article.author"
+          :date="article.published_at"
+        />
+      </div>
+    </div>
+    <div class="flex w-full justify-center">
+      <div
+        v-if="selected"
+        class="w-full flex flex-wrap justify-center gap-3 h-full m-4 pb-8 max-w-[1240px]"
+      >
+        <ArticlesArticlecard
+          v-for="(article, i) in data.data.filter(
+            (articles) => articles.category === selected
+          )"
           :key="i"
           :title="article.title"
           :description="article.description"
@@ -78,11 +133,15 @@
 <script setup>
 import { fetchData } from '~~/helpers/utils';
 
+const input = ref('');
 const data = fetchData();
 
-const input = ref('');
+const selected = ref('');
 
-let search = false;
+const onSelect = (name) => {
+  selected.value = name;
+  console.log(selected.value);
+};
 
 useHead({
   title: 'Kis | Articles',
@@ -113,3 +172,10 @@ useHead({
   ],
 });
 </script>
+
+<style>
+.filtered-category {
+  background-color: rgb(48, 105, 48);
+  color: white;
+}
+</style>
